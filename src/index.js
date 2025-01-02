581af683a68d90f7c2e952a11c92a2274d02301f
MasterQuanh <quanganhdang2004@gmail.com>
2025-01-02 22:16:30 +0700

    refactor code : create routes , app controllers folders

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ const { engine } = require('express-handlebars')
 const path = require('path')
 const app = express()
 const port = 3000
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 // HTTP Logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('.hbs', engine({
@@ -19,14 +24,13 @@ app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'))
 
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+// routes init
+route(app)
+ 
 // 127.0.0.1 - local host
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+// Lấy dữ liệu từ phía client khi gửi qua parameter ta sử dụng req.query
+// Lấy dữ liệu từ phía client khi gửi qua form data ta sử dụng req.body 
