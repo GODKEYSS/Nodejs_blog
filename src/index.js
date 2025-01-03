429fcec45d3da53a2d26d6400df30d5862cff576
MasterQuanh <quanganhdang2004@gmail.com>
2025-01-03 20:50:07 +0700

    install mongodb and add mongoose lib succcessfully

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const path = require('path')
 const app = express()
 const port = 3000
 const route = require('./routes')
+const db = require('./config/db')  // connect to db
+db.connect()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
